308ccdf32f8f3d3a20a4e1fa96eb542b08d2417c
Delilah Adams <d.adams206@gmail.com>
2020-06-20 12:22:25 +0100

    inital commit

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const exphbs = require('express-handlebars');
+const app = express();
+//environment var for port
+const port = process.env.PORT || 3000;
+//setup view engine
+app.engine('handlebars',exphbs({defaultlayout:'main'}));
+app.set('view engine','handlebars');
+
+app.get('/',(req,res)=>{
+	res.render('home',{title:'Home'});
+});
+
+app.get('/about', (req,res)=>{
+	res.render('about',{title:'About'});
+});
+app.get('/contact',(req,res)=>{
+	res.render('contact',{title:'Contact'});
+});
+
+app.listen(port,() =>{
+	console.log('server is runnig on port ${port}');
+});
